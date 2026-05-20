8d77c775c8f6b3257fc5ae140df96ba5b8983a15
GeroA6 <aguerogeronimo954@gmail.com>
2026-05-20 17:30:36 -0300

    Primer ejercicio resuelto.

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,60 @@
+import javax.swing.*;
+import java.awt.event.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // caja principal (ventana)
+        JFrame jFrame = new JFrame();
+
+        jFrame.setSize(800, 500);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // contenedor intermedio (donde van los botones y texto)
+        JPanel jPanel = new JPanel();
+        
+        // el jPanel va dentro del jFrame
+        jFrame.add(jPanel);
+        
+        // componentes dentro del jPanel
+        JLabel jLabel = new JLabel("Introduce tu nombre:");
+
+        JTextField jTextField = new JTextField();
+        jTextField.setColumns(20);
+        
+        JLabel jLabel2 = new JLabel();
+        
+        JButton jButton = new JButton("Saludar");
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // lógica que ocurre cuando se hace clic
+                String nombre = jTextField.getText();
+                jLabel2.setText("¡Hola, " + nombre + "!");
+            }
+        });
+        
+        jPanel.add(jLabel);
+        jPanel.add(jTextField);
+        jPanel.add(jButton);
+        jPanel.add(jLabel2);
+        
+        jFrame.setVisible(true);
+        
+        
+        
+
+        
+        
+
+
+
+
+
+
+
+
+
+
     }
 }
